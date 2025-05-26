26940b4feeeec3ae1bfd989f0be40e289cf5b5ac
Butanol <51089545+Butanol@users.noreply.github.com>
2025-05-26 19:52:49 +0800

    added scraper

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+//const {onRequest} = require("firebase-functions/v2/https");
+//const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -17,3 +17,26 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+const functions = require("firebase-functions");
+
+const scraper = require("./scraper");
+
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+const db = admin.firestore();
+const getData = ()  =>
+{
+    const today = new Date();
+    return `${today.getDate()}${today.getMonth()}${today.getFullYear()}`;
+};
+exports.pubsub = functions.region("asia-southeast1")
+    .runWith({memory: '128MB'})
+    .pubsub.schedule("").timeZone("")
+    .onRun(async () => {
+        try {
+            const data = await scraper.scrapeData();
+        } catch (error) {
+            throw Error(error);
+        }
+    })
